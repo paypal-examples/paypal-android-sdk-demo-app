c2adab1411ca6f930a3ac689c83687448989b702
sshropshire <sshropshire@paypal.com>
2025-03-28 13:38:04 -0500

    Clean up CartView.

--- app/src/main/java/com/firstapp/paypaldemo/main/CartView.kt
+++ app/src/main/java/com/firstapp/paypaldemo/main/CartView.kt
@@ -2,9 +2,25 @@ package com.firstapp.paypaldemo.main
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,9 +29,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.firstapp.paypaldemo.R
 import com.paypal.android.paymentbuttons.PayPalButton
@@ -59,7 +75,7 @@ fun CartView(
             CartItemView(item)
         }
 
-        Divider(
+        HorizontalDivider(
             modifier = Modifier
                 .padding(vertical = 16.dp)
                 .fillMaxWidth(),
@@ -152,7 +168,6 @@ fun CartItemView(item: Item) {
 fun PaymentButton(
     text: String,
     backgroundColor: Color,
-    iconRes: Int? = null,
     onClick: () -> Unit
 ) {
     Button(
@@ -184,8 +199,8 @@ fun CartViewPreview() {
     MaterialTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
             CartView(
-                onPayWithCard = { amount -> },
-                onPayWithPayPal = { amount -> }
+                onPayWithCard = {},
+                onPayWithPayPal = {}
             )
         }
     }
