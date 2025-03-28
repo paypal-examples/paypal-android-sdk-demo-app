eb8251ee9f311e56d5f77fd2dc5fe07f7f68ec1a
sshropshire <sshropshire@paypal.com>
2025-03-28 10:28:47 -0500

    Update SimpleActivity.

--- app/src/main/java/com/firstapp/paypaldemo/SimpleActivity.kt
+++ app/src/main/java/com/firstapp/paypaldemo/SimpleActivity.kt
@@ -1,5 +1,6 @@
 package com.firstapp.paypaldemo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,40 +15,65 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.lifecycleScope
 import com.firstapp.paypaldemo.service.Order
 import com.paypal.android.corepayments.CoreConfig
 import com.paypal.android.corepayments.Environment
 import com.paypal.android.paymentbuttons.PayPalButton
 import com.paypal.android.paymentbuttons.PayPalButtonColor
+import com.paypal.android.paypalwebpayments.PayPalPresentAuthChallengeResult
 import com.paypal.android.paypalwebpayments.PayPalWebCheckoutClient
+import com.paypal.android.paypalwebpayments.PayPalWebCheckoutFinishStartResult
+import com.paypal.android.paypalwebpayments.PayPalWebCheckoutRequest
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 // CartItem.kt
 data class CartItem(val name: String, val price: Double)
 
-// API.kt
-class API {
+// OrderRequest.kt
+data class OrderRequest(val amount: Double)
 
-    suspend fun createOrder(): Result<Order> {
-        // TODO: create
-        return Result.failure(Throwable("could not create order."))
-    }
+// API.kt
+interface API {
+
+    @POST("/orders")
+    suspend fun createOrder(@Body orderRequest: OrderRequest): Response<Order>
+
+    @POST("/orders/{orderId}/capture")
+    suspend fun captureOrder(@Path("orderId") orderId: String): Response<Order>
 }
 
 // SimpleActivity.kt
 class SimpleActivity : ComponentActivity() {
 
+    companion object {
+        fun createAPI(baseUrl: String): API {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .build()
+            return retrofit.create(API::class.java)
+        }
+    }
+
+    // TODO: change to Environment.LIVE when your app is ready for production
+    val environment = Environment.SANDBOX
+    val config = CoreConfig(clientId = "<YOUR_CLIENT_ID_HERE>", environment = environment)
+    val urlScheme = "com.firstapp"
+    val payPalClient =
+        PayPalWebCheckoutClient(context = this, configuration = config, urlScheme = urlScheme)
+
+    val api = createAPI("https://api.myserver.com")
     val cartItems = listOf(
         CartItem(name = "Selkirk Pro Amped Air", price = 179.99),
         CartItem(name = "Joola Ben Johns", price = 229.99)
     )
 
-    // TODO: change to Environment.LIVE when your app is ready for production
-    val environment = Environment.SANDBOX
-    val config = CoreConfig(clientId = "<YOUR_CLIENT_ID_HERE>", environment = environment)
-
-    val urlScheme = "com.firstapp"
-    val payPalClient =
-        PayPalWebCheckoutClient(context = this, configuration = config, urlScheme = urlScheme)
+    var authState: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,16 +81,82 @@ class SimpleActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CartView(modifier = Modifier.padding(innerPadding)) {
-                        launchPayPal()
-                    }
+                    CartView(
+                        modifier = Modifier.padding(innerPadding),
+                        onPayPalButtonClick = { launchPayPal() }
+                    )
                 }
             }
         }
     }
 
-    private fun launchPayPal() {
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val pendingAuthState = authState
+        if (intent != null && pendingAuthState != null) {
+            lifecycleScope.launch {
+                completePayPalAuthChallenge(intent, pendingAuthState)
+            }
+        }
+    }
 
+    private fun launchPayPal() {
+        lifecycleScope.launch {
+            val amount = cartItems.sumOf { it.price }
+            val request = OrderRequest(amount = amount)
+            val response = api.createOrder(request)
+            if (response.isSuccessful) {
+                val order = response.body()
+                presentPayPalAuthChallenge(order = order!!)
+            } else {
+                TODO("handle order creation error")
+            }
+        }
+    }
+
+    private fun presentPayPalAuthChallenge(order: Order) {
+        val request = PayPalWebCheckoutRequest(orderId = order.id)
+        val payPalStartResult =
+            payPalClient.start(activity = this@SimpleActivity, request = request)
+        when (payPalStartResult) {
+            is PayPalPresentAuthChallengeResult.Success -> {
+                // capture auth state
+                authState = payPalStartResult.authState
+            }
+
+            is PayPalPresentAuthChallengeResult.Failure -> TODO("handle paypal start error")
+        }
+    }
+
+    private suspend fun completePayPalAuthChallenge(intent: Intent, state: String) {
+        when (val checkoutResult = payPalClient.finishStart(intent, state)) {
+            is PayPalWebCheckoutFinishStartResult.Success -> {
+                // Capture or authorize order on your server
+                api.captureOrder(checkoutResult.orderId!!)
+                discardAuthState()
+            }
+
+            is PayPalWebCheckoutFinishStartResult.Failure -> {
+                // TODO: Handle approve order failure
+                discardAuthState()
+            }
+
+            is PayPalWebCheckoutFinishStartResult.Canceled -> {
+                // TODO: Notify user PayPal checkout was canceled
+                discardAuthState()
+            }
+
+            PayPalWebCheckoutFinishStartResult.NoResult -> {
+                // There isn't enough information to determine the state of the auth
+                // challenge for this payment method; do nothing to preserve pending
+                // auth state in case the PayPal Auth flow is completed at a later time
+            }
+        }
+    }
+
+    private fun discardAuthState() {
+        // prevent future finish events from being parsed with stale auth state
+        authState = null
     }
 }
 
@@ -79,13 +171,9 @@ fun CartView(modifier: Modifier = Modifier, onPayPalButtonClick: () -> Unit = {}
 fun PayPalButtonView(modifier: Modifier = Modifier, onClick: () -> Unit) {
     AndroidView(
         factory = { context ->
-            PayPalButton(context).apply {
-                setOnClickListener { onClick() }
-            }
+            PayPalButton(context).apply { setOnClickListener { onClick() } }
         },
-        update = { button ->
-            button.color = PayPalButtonColor.BLUE
-        },
+        update = { button -> button.color = PayPalButtonColor.BLUE },
         modifier = modifier
     )
 }
